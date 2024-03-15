bee53929bc81349ca414406db933d2b42e0615b9
LouisMichelin <louis.michelin097@gmail.com>
2024-03-15 19:20:43 +0100

    Dashboard : Setup initial

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import "./Dashboard.scss";
+
+function Dashboard() {
+   return (
+      <div className="DashboardWrapper">
+         <div>DASHBOARD</div>
+         <div>BABYYYYYYYYY</div>
+      </div>
+   );
+}
+
+export default Dashboard;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Lateral from "../components/Lateral/Lateral";
+import Dashboard from "../components/Dashboard/Dashboard";
 
 function App() {
    return (
       <div className="AppWrapper">
          <Lateral />
          {/* <Board /> */}
+         <Dashboard />
       </div>
    );
 }
