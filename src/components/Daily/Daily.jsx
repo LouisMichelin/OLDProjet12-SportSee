1c99f4840efe7d51e02b70d2071bf5111e8dd856
LouisMichelin <louis.michelin097@gmail.com>
2024-03-17 12:56:51 +0100

    More Edits of Graph : no vertical grid

--- src/components/Daily/Daily.jsx
+++ src/components/Daily/Daily.jsx
@@ -13,6 +13,7 @@ import {
    BarChart,
    Bar,
    Rectangle,
+   ReferenceLine,
    XAxis,
    YAxis,
    CartesianGrid,
@@ -20,114 +21,120 @@ import {
 
 function Daily({ userId }) {
    // ---------------------------------------------------------------
-   // const [data, setData] = useState(null);
-   // const [loading, setLoading] = useState(true);
-   // const [error, setError] = useState(null);
-   // useEffect(() => {
-   //    fetch(`http://localhost:1337/user/${userId}/activity`)
-   //       .then((response) => {
-   //          if (response.ok) {
-   //             return response.json();
-   //          }
-   //          throw response;
-   //       })
-   //       .then((data) => {
-   //          setData(data);
-   //          console.log(data);
-   //       })
-   //       .catch((error) => {
-   //          console.error("Error fetching data : ", error);
-   //          setError(error);
-   //       })
-   //       .finally(() => {
-   //          setLoading(false);
-   //       });
-   // }, [userId]);
-   // if (loading) return "Loading...";
-   // if (error) return "Error!";
-   // const userData = data.data;
-   // const userSessions = userData.sessions;
+   const [data, setData] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+   useEffect(() => {
+      fetch(`http://localhost:1337/user/${userId}/activity`)
+         .then((response) => {
+            if (response.ok) {
+               return response.json();
+            }
+            throw response;
+         })
+         .then((data) => {
+            setData(data);
+            // console.log(data);
+         })
+         .catch((error) => {
+            console.error("Error fetching data : ", error);
+            setError(error);
+         })
+         .finally(() => {
+            setLoading(false);
+         });
+   }, [userId]);
+   if (loading) return "Loading...";
+   if (error) return "Error!";
+   const userData = data.data;
+   const userSessions = userData.sessions;
+   // console.log(userSessions);
    // ---------------------------------------------------------------
-   const data = [
-      {
-         day: 1,
-         poids: 80,
-         calories: 240,
-         // amt: 75,
-      },
-      {
-         day: 2,
-         poids: 80,
-         calories: 220,
-         // amt: 75,
-      },
-      {
-         day: 3,
-         poids: 75,
-         calories: 280,
-         // amt: 75,
-      },
-      {
-         day: 4,
-         poids: 70,
-         calories: 200,
-         // amt: 75,
-      },
-      {
-         day: 5,
-         poids: 65,
-         calories: 150,
-         // amt: 75,
-      },
-      {
-         day: 6,
-         poids: 65,
-         calories: 300,
-         // amt: 75,
-      },
-      {
-         day: 7,
-         poids: 70,
-         calories: 200,
-         // amt: 75,
-      },
-      {
-         day: 8,
-         poids: 65,
-         calories: 150,
-         // amt: 75,
-      },
-      {
-         day: 9,
-         poids: 65,
-         calories: 300,
-         // amt: 75,
-      },
-      {
-         day: 10,
-         poids: 65,
-         calories: 300,
-         // amt: 75,
-      },
-   ];
+
+   // ---------------------------------------------------------------
+   // CE DATA LA ======> données fixes pour exemples
+   // const data = [
+   //    {
+   //       day: 1,
+   //       poids: 80,
+   //       calories: 240,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 2,
+   //       poids: 80,
+   //       calories: 220,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 3,
+   //       poids: 75,
+   //       calories: 280,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 4,
+   //       poids: 70,
+   //       calories: 200,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 5,
+   //       poids: 65,
+   //       calories: 150,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 6,
+   //       poids: 65,
+   //       calories: 300,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 7,
+   //       poids: 70,
+   //       calories: 200,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 8,
+   //       poids: 65,
+   //       calories: 150,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 9,
+   //       poids: 65,
+   //       calories: 300,
+   //       // amt: 75,
+   //    },
+   //    {
+   //       day: 10,
+   //       poids: 65,
+   //       calories: 300,
+   //       // amt: 75,
+   //    },
+   // ];
+   const graphData = [];
+   userSessions.forEach((element, index) => {
+      graphData.push({
+         day: index,
+         "Poids (kg)": element.kilogram,
+         "Calories brûlées (kCal)": element.calories,
+      });
+   });
+
+   console.log("graphdata = ", graphData);
+
    return (
       <div className="DailyWrapper">
          <>
-            <div className="DailyTitle">Mon Titre de Graph</div>
-            {/* {userSessions.map((activity) => {
-               return (
-                  <div key={Math.random()}>
-                     {activity.day} - {activity.kilogram} - {activity.calories}
-                  </div>
-               );
-            })} */}
-
-            {/* <Bar className="DailyBar" data={barData} /> */}
+            <div className="DailyTitle">Activité quotidienne</div>
             <ResponsiveContainer width="100%" height="100%">
                <BarChart
                   width={700}
                   height={300}
-                  data={data}
+                  data={graphData}
                   margin={{
                      top: 30,
                      right: 10,
@@ -142,27 +149,33 @@ function Daily({ userId }) {
                      align="right"
                      height={50}
                   />
-                  <CartesianGrid strokeDasharray="3 3" />
+                  <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                  <ReferenceLine x="0" />
                   <XAxis dataKey="day" />
                   <YAxis
                      orientation="right"
                      type="number"
                      domain={[0, "auto"]}
+                     interval={1}
+                     // dataKey="Poids (kg)"
                   />
                   <Tooltip />
                   <Bar
-                     dataKey="poids"
+                     dataKey="Poids (kg)"
                      fill="black"
                      barSize={10}
-                     radius={[4, 4, 0, 0]}
+                     radius={[4.5, 4.5, 0, 0]}
                      paddingRight={5}
+                     scaling={1}
                      // activeBar={<Rectangle fill="grey" stroke="blue" />}
                   />
                   <Bar
-                     dataKey="calories"
+                     dataKey="Calories brûlées (kCal)"
                      fill="red"
                      barSize={10}
-                     radius={[4, 4, 0, 0]}
+                     radius={[4.5, 4.5, 0, 0]}
+                     scaling={0.5}
+
                      // activeBar={<Rectangle fill="orange" stroke="purple" />}
                   />
                </BarChart>
